9b62e52bf59ac0a68beb310d4402be676bbdf644
Dessy Owiti <owitidessy@yahoo.com>
2019-10-24 11:17:56 +0000

    Two files for Deck challenge, Card deck file was edited by my mentor so all cards are delt

--- CardDeckRevised.rb
+++ CardDeckRevised.rb
@@ -17,6 +17,7 @@ end
 
 
 class Deck
+  attr_reader :cards
 
   def initialize 
     @ranks = [*(2..10), "J", "Q", "K", "A"] #1st subarray, create an array of card ranks 2 through 10 and with the letters j,q,k,and a
@@ -39,6 +40,10 @@ class Deck
     @cards.shift
   end  
 
+  def remaining
+    puts @cards.length
+  end
+
   def output
     @cards.each do |card|
       card.output_card
@@ -47,8 +52,17 @@ class Deck
 end
 
 deck = Deck.new 
-deck.shuffle
-deck.deal
-deck.output
+#deck.shuffle
+#deck.deal
+#deck.remaining
 
+# Code 60 to 64 is a while loop that prints remaining cards then deals one card then prints remaining cards.Now cards will continue being dealt until no more cards are left.
+while deck.cards != [] #loop continues until the array is empty
+  deck.remaining 
+  deck.cards.first.output_card
+  deck.deal
+end
+
+#deck.output
+#puts deck.cards.length
 #Card.random_card.output_card
